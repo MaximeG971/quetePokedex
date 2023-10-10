073ac1c4e1185994ca2a184df054a5cdd883d723
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-10-10 11:45:41 +0200

    React quête 5 - passer des props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,17 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-const PokemonCard = () => {
-    const pokemon = pokemonList[0]
+const PokemonCard = (props) => {
+    const { name, imgSrc } = props.pokemon;
     return (
+        <div>
         <figure>
-            {pokemon.imgSrc ? (
-                <img src= {pokemon.imgSrc}></img>
+            {imgSrc ? (
+                <img src= {imgSrc}></img>
             ) : (
                 <p>???</p>
             )
             }
-            <figcaption>{pokemon.name}</figcaption>
+            <figcaption>{name}</figcaption>
 </figure>
+</div>
     )
 }
 
