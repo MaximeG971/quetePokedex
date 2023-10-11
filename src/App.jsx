66f435e3e7f4a51801993a634f2aab4029a41ca3
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-10-11 12:01:42 +0200

    React quete 6 - utiliser un state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -7,16 +8,45 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
-function App() {
-  const pokemon = pokemonList[0]
+const App = () => {
+  const [indexPokemon, setIndexPokemon] = useState(0);
+  const pokemonArray = pokemonList[indexPokemon]
+  const nextClick = () => {
+    if (indexPokemon < pokemonList.length -1) {
+      setIndexPokemon(indexPokemon +1)
+    }
+  }
+  const previousClick = () => {
+    if (indexPokemon > 0) {
+      setIndexPokemon(indexPokemon -1)
+    }
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemon}/>
-    </div>
+      <PokemonCard pokemon={pokemonArray}/>
+      <button onClick={previousClick}>Précédent</button>
+      <button onClick={nextClick}>Suivant</button>
+      </div>
   )
 }
 
