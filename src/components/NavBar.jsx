10bf1e321a55ec1a6dc717bc7e7e80bada60489d
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-10-12 12:01:55 +0200

    React quête 8 - Répéter un composant avec map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,15 @@
-const NavBar = ({indexPokemon, setIndexPokemon, pokemonList}) => {
-    const nextClick = () => {
-        if (indexPokemon < pokemonList.length -1) {
-          setIndexPokemon(indexPokemon +1)
-        }
-      }
-      const previousClick = () => {
-        if (indexPokemon > 0) {
-          setIndexPokemon(indexPokemon -1)
-        }
-      }
-
+const NavBar = ({setIndexPokemon, pokemonList}) => {
       return (
         <div>
-          <button onClick={previousClick}>Précédent</button>
-          <button onClick={nextClick}>Suivant</button>
-          </div>
+            {pokemonList.map((pokemon, index) => (
+                <button
+                key={index}
+                onClick={() => setIndexPokemon(index)}
+                >
+                    {pokemon.name}
+                </button>
+            ))}
+        </div>
       )
 };
 
